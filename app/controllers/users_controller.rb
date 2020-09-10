1d04ad88964ab4b18a8ce0c11087ee8162d83876
Casey Yost <casey.s.yost@gmail.com>
2020-09-10 14:04:58 -0700

    more admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    @articles = @user.articles
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 5)
+    @users = User.all
   end
 
   def new
@@ -51,5 +52,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
   end
 end
